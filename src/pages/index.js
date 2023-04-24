82acd03cb66ded63eca3425a1b3b9a89ba2dabac
Rivan Chandra <rivan@genesisdigital.asia>
2023-04-24 21:27:59 +0700

    add float button at the bottom middle of the screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import BackgroundVideo from './BackgroundVideo';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Home() {
   const [videoUrl, setVideoUrl] = useState('/videos/1-day.mp4');
@@ -7,6 +9,9 @@ export default function Home() {
   return (
     <>
       <BackgroundVideo video={videoUrl} />
+      <Fab sx={{ position: 'fixed', bottom: '50px', left: '50%', transform: 'translateX(-50%)' }} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
     </>
   )
 }
