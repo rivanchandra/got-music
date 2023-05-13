252a3e90f6dad5debbc95ea767afb9e345c0c1c2
Rivan Chandra <rivan@genesisdigital.asia>
2023-05-14 01:35:25 +0700

    update music list

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,8 +34,12 @@ let count = 0;
 
 const musicList = [
   {
-    name: 'Main Title',
-    url: 'https://youtu.be/Ov5ljc44Ajs'
+    name: 'Game Of Thrones',
+    url: 'https://youtu.be/9VgoADESpw0'
+  },
+  {
+    name: 'A Raven From Kings Landing',
+    url: 'https://youtu.be/AXb5wyNf6Jw'
   },
   {
     name: 'North Of The Wall',
@@ -46,60 +50,76 @@ const musicList = [
     url: 'https://youtu.be/spQljapBR7s'
   },
   {
-    name: 'The Kingsroad',
-    url: 'https://youtu.be/60OCZAHSxDk'
-  },
-  {
     name: 'The Kings Arrival',
     url: 'https://youtu.be/PwsUAxu6rjg'
-  },
-  {
-    name: 'Love In The Eyes',
-    url: 'https://youtu.be/SFbp8GzZ-ec'
-  },
-  {
-    name: 'A Raven From Kings Landing',
-    url: 'https://youtu.be/AXb5wyNf6Jw'
-  },
-  {
-    name: 'The Wall',
-    url: 'https://youtu.be/eRgxqnP0qw4'
-  },
-  {
-    name: 'Things I Do For Love',
-    url: 'https://youtu.be/JZyLqrVfslQ'
   },
   {
     name: 'A Golden Crown',
     url: 'https://youtu.be/hX509eFmZfg'
   },
   {
+    name: 'Love In The Eyes',
+    url: 'https://youtu.be/SFbp8GzZ-ec'
+  },
+  {
+    name: 'Main Title',
+    url: 'https://youtu.be/Ov5ljc44Ajs'
+  },
+  {
+    name: 'Things I Do For Love',
+    url: 'https://youtu.be/JZyLqrVfslQ'
+  },
+  {
     name: 'Winter Is Coming',
     url: 'https://youtu.be/jHWk1bFwqYE'
+  },
+  {
+    name: 'To Vaes Dothrak',
+    url: 'https://youtu.be/MrnMIIMRC0g'
   },
   {
     name: 'A Bird Without Feathers',
     url: 'https://youtu.be/1Lc1skF69lU'
   },
   {
-    name: 'Await The Kings Justice',
-    url: 'https://youtu.be/fON6yJyM1bw'
-  },
-  {
-    name: 'Youll Be Queen One Day',
-    url: 'https://youtu.be/UaPGoXe3VLo'
+    name: 'The Kingsroad',
+    url: 'https://youtu.be/60OCZAHSxDk'
   },
   {
     name: 'The Assassins Dagger',
     url: 'https://youtu.be/bCuuWufKPPk'
   },
   {
-    name: 'Jons Honor',
-    url: 'https://youtu.be/HFIWi-KoozE'
+    name: 'Await The Kings Justice',
+    url: 'https://youtu.be/fON6yJyM1bw'
   },
   {
-    name: 'To Vaes Dothrak',
-    url: 'https://youtu.be/MrnMIIMRC0g'
+    name: 'The North Remembers',
+    url: 'https://youtu.be/8lS7hOAizSA'
+  },
+  {
+    name: 'The Nights Watch',
+    url: 'https://youtu.be/KU6G0rvLydc'
+  },
+  {
+    name: 'The Old Gods And The New',
+    url: 'https://youtu.be/h7rZEBoN1fo'
+  },
+  {
+    name: 'The Wall',
+    url: 'https://youtu.be/eRgxqnP0qw4'
+  },
+  {
+    name: 'The Pointy End',
+    url: 'https://youtu.be/ulriP5SBStY'
+  },
+  {
+    name: 'Small Pack Of Wolves',
+    url: 'https://youtu.be/avNYe_941so'
+  },
+  {
+    name: 'Youll Be Queen One Day',
+    url: 'https://youtu.be/UaPGoXe3VLo'
   },
   {
     name: 'Black Of Hair',
@@ -110,72 +130,148 @@ const musicList = [
     url: 'https://youtu.be/S0LG82B1bVc'
   },
   {
-    name: 'Small Pack Of Wolves',
-    url: 'https://youtu.be/avNYe_941so'
-  },
-  {
-    name: 'Game Of Thrones',
-    url: 'https://youtu.be/9VgoADESpw0'
-  },
-  {
-    name: 'Kill Them All',
-    url: 'https://youtu.be/CWm1jRbMB0M'
-  },
-  {
-    name: 'The Pointy End',
-    url: 'https://youtu.be/ulriP5SBStY'
+    name: 'Jons Honor',
+    url: 'https://youtu.be/HFIWi-KoozE'
   },
   {
     name: 'Victory Does Not Make Us Conquerors',
     url: 'https://youtu.be/wbSYRVese6Q'
   },
   {
-    name: 'When The Sun Rises In The West',
-    url: 'https://youtu.be/Rl3HQsJjsHI'
+    name: 'Kill Them All',
+    url: 'https://youtu.be/CWm1jRbMB0M'
   },
   {
     name: 'King Of The North',
     url: 'https://youtu.be/t4U66h3robc'
   },
   {
-    name: 'The Nights Watch',
-    url: 'https://youtu.be/KU6G0rvLydc'
-  },
-  {
-    name: 'Fire And Blood',
-    url: 'https://youtu.be/RxBgXtwBHrE'
+    name: 'When The Sun Rises In The West',
+    url: 'https://youtu.be/Rl3HQsJjsHI'
   },
   {
     name: 'Finale',
     url: 'https://youtu.be/jh_d5fvO0NU'
   },
   {
-    name: 'A Lannister Always Pays His Debts',
-    url: 'https://youtu.be/5qNtmne62Vo'
+    name: 'We Are The Watchers On The Wall',
+    url: 'https://youtu.be/YUu76kATaJI'
+  },
+  {
+    name: 'Warrior Of Light',
+    url: 'https://youtu.be/TZ64AkvFB9o'
+  },
+  {
+    name: 'The Throne Is Mine',
+    url: 'https://youtu.be/VC_O0vqdO-c'
+  },
+  {
+    name: 'What Is Dead May Never Die',
+    url: 'https://youtu.be/PSAvJCs3K8E'
+  },
+  {
+    name: 'White Walkers',
+    url: 'https://youtu.be/AsKQF20vUEo'
+  },
+  {
+    name: 'I Am Hers, She Is Mine',
+    url: 'https://youtu.be/Iu4bVyxKS8g'
+  },
+  {
+    name: 'Qarth',
+    url: 'https://youtu.be/m5l8nU0bVjA'
+  },
+  {
+    name: 'I Will Keep You Safe',
+    url: 'https://youtu.be/qugKrVCzgLk'
+  },
+  {
+    name: 'Valar Morghulis',
+    url: 'https://youtu.be/SetTHx-8QSk'
+  },
+  {
+    name: 'Pay The Iron Price',
+    url: 'https://youtu.be/UKSYgs7TUZo'
+  },
+  {
+    name: 'Pyat Pree',
+    url: 'https://youtu.be/FiNcqNdxU3o'
+  },
+  {
+    name: 'Winterfell',
+    url: 'https://youtu.be/fyY8H6KYIkw'
+  },
+  {
+    name: 'Wildfire',
+    url: 'https://youtu.be/PhqYJY6jRjo'
+  },
+  {
+    name: 'Dont Die With A Clean Sword',
+    url: 'https://youtu.be/47rojSGiUlk'
+  },
+  {
+    name: 'Stand And Fight',
+    url: 'https://youtu.be/PQPkdfuCUZg'
+  },
+  {
+    name: 'One More Drink Before The War',
+    url: 'https://youtu.be/0uP01EVQWOQ'
+  },
+  {
+    name: 'The Rains Of Castomere',
+    url: 'https://youtu.be/9vDL7AgLdYQ'
+  },
+  {
+    name: 'House Of The Undying',
+    url: 'https://youtu.be/cGnurJ1a5e4'
+  },
+  {
+    name: 'Mother Of Dragons',
+    url: 'https://youtu.be/Yh4798a0M-M'
+  },
+  {
+    name: 'Three Blasts',
+    url: 'https://youtu.be/GBn-8x1gzXM'
+  },
+  {
+    name: 'The Night Is Dark',
+    url: 'https://youtu.be/2LGTCfzQIDY'
   },
   {
     name: 'Dracarys',
-    url: 'https://youtu.be/jIsla-hNxf0'
+    url: 'https://youtu.be/dvEdljiAqe0'
   },
   {
-    name: 'I Paid the Iron Price',
-    url: 'https://youtu.be/D0-ptcQc1I8'
+    name: 'Heir To Winterfell',
+    url: 'https://youtu.be/ChQ-vA0x9r8'
   },
   {
-    name: 'Chaos Is a Ladder',
-    url: 'https://youtu.be/sTe_hPK2RHY'
+    name: 'Oathkeeper',
+    url: 'https://youtu.be/bob6tIGhfbo'
   },
   {
-    name: 'Dark Wings, Dark Words',
-    url: 'https://youtu.be/fywMObuBTrI'
+    name: 'Crasters Keep',
+    url: 'https://youtu.be/s94jAzUX48k'
+  },
+  {
+    name: 'Mereen',
+    url: 'https://youtu.be/mz9dL3_CjwY'
+  },
+  {
+    name: 'Reek',
+    url: 'https://youtu.be/9miyyd9_bUE'
   },
   {
     name: 'You Know Nothing',
     url: 'https://youtu.be/yd8IMTzc2bI'
   },
   {
-    name: 'Wall Of Ice',
-    url: 'https://youtu.be/XrAUlIdmcZ8'
+    name: 'The Night Is Dark',
+    url: 'https://youtu.be/snP0o-jUs4g'
+  },
+  {
+    name: 'Its Always Summer Under the Sea',
+    url: 'https://youtu.be/aqhqXPlZPkY'
   },
   {
     name: 'Kingslayer',
@@ -186,36 +282,196 @@ const musicList = [
     url: 'https://youtu.be/ps_6VVtPDQU'
   },
   {
-    name: 'White Walkers',
-    url: 'https://youtu.be/AsKQF20vUEo'
+    name: 'Wall Of Ice',
+    url: 'https://youtu.be/XrAUlIdmcZ8'
   },
   {
-    name: 'Its Always Summer Under the Sea',
-    url: 'https://youtu.be/aqhqXPlZPkY'
-  },
-  {
-    name: 'Reek',
-    url: 'https://youtu.be/9miyyd9_bUE'
-  },
-  {
-    name: 'The Night Is Dark',
-    url: 'https://youtu.be/snP0o-jUs4g'
-  },
-  {
-    name: 'The Lannisters Send Their Regards',
-    url: 'https://youtu.be/H4purJGrRfg'
+    name: 'I Paid the Iron Price',
+    url: 'https://youtu.be/D0-ptcQc1I8'
   },
   {
     name: 'Heir to Winterfell',
     url: 'https://youtu.be/eXbWn_VyPPU'
   },
   {
+    name: 'Chaos Is a Ladder',
+    url: 'https://youtu.be/sTe_hPK2RHY'
+  },
+  {
+    name: 'First of His Name',
+    url: 'https://youtu.be/XgN6dHpWYyU'
+  },
+  {
+    name: 'A Lannister Always Pays His Debts',
+    url: 'https://youtu.be/5qNtmne62Vo'
+  },
+  {
+    name: 'The Lannisters Send Their Regards',
+    url: 'https://youtu.be/H4purJGrRfg'
+  },
+  {
     name: 'Mhysa',
     url: 'https://youtu.be/ggS5zjraJi4'
   },
   {
-    name: 'For the Realm',
-    url: 'https://youtu.be/0_yQa83T8Qg'
+    name: 'Two Swords',
+    url: 'https://youtu.be/9dRh2MjeoP8'
+  },
+  {
+    name: 'Jaws of the Viper',
+    url: 'https://youtu.be/jFff6RHBKZA'
+  },
+  {
+    name: 'Breaker of Chains',
+    url: 'https://youtu.be/u04paWXpTTk'
+  },
+  {
+    name: 'The Bear and the Maiden Fair',
+    url: 'https://youtu.be/cIbZYFtkE0c'
+  },
+  {
+    name: 'Take Charge of Your Life',
+    url: 'https://youtu.be/1F5mXjHsgz0'
+  },
+  {
+    name: 'I Only See What Matters',
+    url: 'https://youtu.be/fKZZj66K9C0'
+  },
+  {
+    name: 'Thenns',
+    url: 'https://youtu.be/rM5Q1iL27ws'
+  },
+  {
+    name: 'Dark Wings, Dark Words',
+    url: 'https://youtu.be/9hO_8uwK6l8'
+  },
+  {
+    name: 'Three Eyed Raven',
+    url: 'https://youtu.be/g-WIVGCR6WU'
+  },
+  {
+    name: 'Unbowed, Unbent, Unbroken',
+    url: 'https://youtu.be/AZkXeTQ3n4A'
+  },
+  {
+    name: 'Im Sorry for Today',
+    url: 'https://youtu.be/aKDm3jHbMrk'
+  },
+  {
+    name: 'Forgive Me',
+    url: 'https://youtu.be/jq-kj30PGc0'
+  },
+  {
+    name: 'The Biggest Fire the North Has Ever Seen',
+    url: 'https://youtu.be/ooc7y9Dwuyc'
+  },
+  {
+    name: 'Lets Kill Some Crows',
+    url: 'https://youtu.be/yUqdYpT7Vbs'
+  },
+  {
+    name: 'Watchers on the Wall',
+    url: 'https://youtu.be/80iDZiAjkXk'
+  },
+  {
+    name: 'Mothers Mercy',
+    url: 'https://youtu.be/ECeNNnE2fJY'
+  },
+  {
+    name: 'I Only See What Matters',
+    url: 'https://youtu.be/fKZZj66K9C0'
+  },
+  {
+    name: 'The Real North',
+    url: 'https://youtu.be/D8q_GSVx6tw'
+  },
+  {
+    name: 'He Is Lost',
+    url: 'https://youtu.be/qmmIg78NhAE'
+  },
+  {
+    name: 'You Are No Son of Mine',
+    url: 'https://youtu.be/uuD1etH8QL0'
+  },
+  {
+    name: 'The Children',
+    url: 'https://youtu.be/_JYKNqvsbKw'
+  },
+  {
+    name: 'Atonement',
+    url: 'https://youtu.be/O7wN-nPnY0g'
+  },
+  {
+    name: 'Kneel for No Man',
+    url: 'https://youtu.be/Z1ADHMg2fa4'
+  },
+  {
+    name: 'High Sparrow',
+    url: 'https://youtu.be/Awat3O078kg'
+  },
+  {
+    name: 'The Wars to Come',
+    url: 'https://youtu.be/nen3jbHgMUM'
+  },
+  {
+    name: 'Son of the Harpy',
+    url: 'https://youtu.be/_8o9cEMrKz0'
+  },
+  {
+    name: 'Blood of the Dragon',
+    url: 'https://youtu.be/-VDh9sQmdQI'
+  },
+  {
+    name: 'House of Black and White',
+    url: 'https://youtu.be/nApFAqy6TP0'
+  },
+  {
+    name: 'Kill the Boy',
+    url: 'https://youtu.be/-oal7VfF8sU'
+  },
+  {
+    name: 'I Dreamt I Was Old',
+    url: 'https://youtu.be/yWowYQfMKCU'
+  },
+  {
+    name: 'Before the Old Gods',
+    url: 'https://youtu.be/BzYhumcppZU'
+  },
+  {
+    name: 'Lets Play a Game',
+    url: 'https://youtu.be/0tjzO7CL6bI'
+  },
+  {
+    name: 'I Need You by My Side',
+    url: 'https://youtu.be/Rt94_QVxyxg'
+  },
+  {
+    name: 'Feed the Hounds',
+    url: 'https://youtu.be/ddXHkHO8E3A'
+  },
+  {
+    name: 'Hardhome, Pt. 1',
+    url: 'https://youtu.be/itG4xtASwYw'
+  },
+  {
+    name: 'Hardhome, Pt. 2',
+    url: 'https://youtu.be/jF0ImzXSZas'
+  },
+  {
+    name: 'Dance Of Dragons',
+    url: 'https://youtu.be/cqTRHblVB4Y'
+  },
+  {
+    name: 'A Painless Death',
+    url: 'https://youtu.be/exJ-9al2wRE'
+  },
+  {
+    name: 'I Choose Violence',
+    url: 'https://youtu.be/Rgh7CftYXkY'
+  },
+  {
+    name: 'My Watch Has Ended',
+    url: 'https://youtu.be/qhEQFFNfl3k'
   }
 ]
 
