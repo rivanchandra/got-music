cf846bb9519ea32c814b57cb4d824e79bb59dea1
Rivan Chandra <rivan@genesisdigital.asia>
2023-05-14 20:11:44 +0700

    Add change background feature

---
+++ src/resources/pictures.js
@@ -0,0 +1,49 @@
+import ShareIcon from '@mui/icons-material/Share';
+
+export const pictures = [
+    {
+        name: 'Arryn',
+        path: '/images/arryn.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Barratheon',
+        path: '/images/barratheon.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Greyjoy',
+        path: '/images/greyjoy.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Lannister',
+        path: '/images/lannister.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Martell',
+        path: '/images/martell.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Stark',
+        path: '/images/stark.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Targarean',
+        path: '/images/targarean.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Tully',
+        path: '/images/tully.jpg',
+        icon: <ShareIcon />
+    },
+    {
+        name: 'Tyrell',
+        path: '/images/tyrell.jpg',
+        icon: <ShareIcon />
+    }
+]
