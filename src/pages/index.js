a8e2f6f0cea2664346a0b0278b06589718112eff
Rivan Chandra <rivan@genesisdigital.asia>
2023-05-15 20:55:32 +0700

    update the drawer layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,10 @@ import ShuffleIcon from '@mui/icons-material/Shuffle';
 import ListIcon from '@mui/icons-material/List';
 
 import screenfull from 'screenfull';
+
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 import Typography from '@mui/material/Typography';
@@ -174,7 +178,22 @@ export default function Home() {
         open={drawer}
         onClose={()=>setDrawer(false)}
       >
-        <p>test4</p>
+        <Box component="span" sx={{ p: 2, width: [300, 500, 1000] }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+            <iframe height="315" src="https://www.youtube.com/embed/bjqEWgDVPe0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </Grid>
+            <Grid item xs={4}>
+              <p>xs=4</p>
+            </Grid>
+            <Grid item xs={4}>
+              <p>xs=4</p>
+            </Grid>
+            <Grid item xs={8}>
+              <p>xs=8</p>
+            </Grid>
+          </Grid>
+        </Box>
       </Drawer>
       <ReactPlayer
         url={currentLive}
