96cecb4bb385c1fe5e2bdf07b5b30e2d64e5a8bd
Rivan Chandra <rivan@genesisdigital.asia>
2023-05-26 21:05:44 +0700

    Update season 3 story

--- src/resources/story.js
+++ src/resources/story.js
@@ -26,6 +26,18 @@ export const gotStory =
         "In the North, Jon Snow, now a member of the Night's Watch, ventures beyond the Wall with a group of fellow brothers to investigate reports of a growing threat from the mysterious and deadly creatures known as White Walkers. Jon forms a complex relationship with the wildling woman Ygritte and must make difficult choices as he grapples with loyalty, honor, and the impending danger of the supernatural forces.",
         "Throughout the season, alliances are forged, betrayals occur, and battles are fought. The realm is plunged deeper into turmoil as the different factions clash and pursue their own ambitions. As the season comes to a close, major events unfold, including the Battle of the Blackwater, which has significant consequences for the power dynamics in Westeros."
       ]
+    },
+    {
+      trailer: "https://www.youtube.com/embed/AlhHGUfCYw4",
+      bts: "https://www.youtube.com/embed/1IKfzFlN4Os",
+      stories: [
+        "Robb Stark, the King in the North, faces challenges in his campaign against House Lannister. Despite some early victories, Robb's position becomes increasingly precarious as betrayals and strategic setbacks plague his cause. The consequences of his choices and alliances have far-reaching implications for both himself and his family.",
+        "In King's Landing, Tyrion Lannister, as Hand of the King, finds himself grappling with the aftermath of the Battle of the Blackwater. As the city recovers from the conflict, Tyrion must outmaneuver his enemies and navigate the treacherous court politics to maintain his influence and protect his family's interests. His relationship with his sister Cersei and father Tywin grows more strained, and he forms unexpected alliances to secure his position.",
+        "Meanwhile, across the Narrow Sea, Daenerys Targaryen continues her quest to build an army and reclaim the Iron Throne. She acquires Unsullied, an elite slave soldier army, and gains the loyalty of the freed slaves of Astapor. Daenerys emerges as a powerful and influential leader, inspiring hope and devotion among her followers, as she sets her sights on the cities of Yunkai and Meereen.",
+        "Beyond the Wall, Jon Snow infiltrates the wildlings under the command of the charismatic Mance Rayder, hoping to gather intelligence on their plans. He forms a complicated relationship with Ygritte, who challenges his loyalties and forces him to question his own identity and values. Jon's experiences with the wildlings and his encounters with the mysterious White Walkers reveal the imminent threat that looms over Westeros.",
+        "In other parts of the realm, the surviving Stark children, Arya, Sansa, and Bran, navigate their own paths and face their own trials. Arya, disguised as a boy, travels with a group led by the enigmatic Jaqen H'ghar, while Sansa remains a captive in King's Landing, enduring the cruelty of Joffrey Baratheon. Bran, accompanied by his loyal companions, explores his newfound abilities beyond the Wall.",
+        "Throughout the season, alliances shift, unexpected alliances form, and key characters face shocking deaths and betrayals. The game of thrones becomes increasingly complex and dangerous as multiple factions clash for power and survival. The third season sets the stage for major turning points and pivotal moments that will have lasting consequences in the tumultuous world of Westeros."
+      ]
     }
   ]
 }
