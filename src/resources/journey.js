c60ab5585e832d265a33a355f592ceee07883dbe
Rivan Chandra <rivan@genesisdigital.asia>
2023-06-01 20:58:30 +0700

    Update Tyrion journey

--- src/resources/journey.js
+++ src/resources/journey.js
@@ -111,31 +111,37 @@ export const journey = [
 		location: [
 			{
 				longitude: 14.54950,
-				latitude: 26.63297
+				latitude: 26.63297,
+				desc: "Tyrion starts the series as a member of House Lannister, one of the most powerful families in Westeros. He primarily resides in King's Landing, the capital city, where he serves as the Hand of the King for his father, Tywin Lannister. He becomes involved in the political machinations of the court and develops relationships with various characters."
 			},
 			{
 				longitude: 18.85336,
-				latitude: 34.90465
+				latitude: 34.90465,
 			},
 			{
 				longitude: 19.07001,
-				latitude: 3.97575
+				latitude: 3.97575,
+				desc: "Tyrion continues to play a significant role in King's Landing, working to defend the city against Stannis Baratheon's forces during the Battle of Blackwater. He outmaneuvers his sister, Cersei Lannister, and serves as the acting Hand of the King while his father is away."
 			},
 			{
 				longitude: 31.11133,
-				latitude: 4.65167
+				latitude: 4.65167,
+				desc: "Tyrion faces further challenges in King's Landing, dealing with the aftermath of the battle and the influence of his family members. He becomes involved in a marriage plot orchestrated by his father, leading to his wedding to Sansa Stark. Tyrion's relationship with his sister deteriorates, and he starts to question his family's actions."
 			},
 			{
 				longitude: 43.63915,
-				latitude: -9.21022
+				latitude: -9.21022,
+				desc: "Tyrion's marriage to Sansa becomes increasingly strained, and he finds solace in a relationship with Shae, a former prostitute. He is accused of poisoning his nephew, King Joffrey, and faces a trial for his life. Eventually, he demands a trial by combat."
 			},
 			{
 				longitude: 67.12768,
-				latitude: -9.59659
+				latitude: -9.59659,
+				desc: "Tyrion escapes from King's Landing with the help of his brother, Jaime Lannister. He travels to Essos, where he crosses paths with Daenerys Targaryen and becomes involved in her council as an advisor. He tries to navigate the political landscape of Meereen."
 			},
 			{
 				longitude: 24.34639,
-				latitude: 7.05785
+				latitude: 7.05785,
+				desc: "Tyrion continues to serve Daenerys as she conquers cities and seeks alliances in Essos. He becomes the Hand of the Queen and helps govern Meereen in Daenerys's absence. He forms alliances with other powerful figures and works towards establishing peace and order."
 			}
 		]
 	},
