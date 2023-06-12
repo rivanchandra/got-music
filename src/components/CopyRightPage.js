ec56b975e8151b756bc8ad8f7bd4d4ef2ad74c22
Rivan Chandra <rivan@genesisdigital.asia>
2023-06-12 17:27:34 +0700

    Update copyright

--- src/components/CopyRightPage.js
+++ src/components/CopyRightPage.js
@@ -3,19 +3,25 @@ import Fab from '@mui/material/Fab';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
+import Box from '@mui/material/Box';
+import Popper from '@mui/material/Popper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Fade from '@mui/material/Fade';
+import Paper from '@mui/material/Paper';
+
 import Tooltip from '@mui/material/Tooltip';
 
 export default function CopyRightPage() {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState();
 
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event) => {
+  const handleClick = (newPlacement) => (event) => {
     setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+    setOpen((prev) => placement !== newPlacement || !prev);
+    setPlacement(newPlacement);
   };
 
   return(
@@ -29,7 +35,7 @@ export default function CopyRightPage() {
             border: 'solid #ffffff 2px',
             backgroundColor: 'rgba(0,0,0,0)',
             position: 'fixed', bottom: '30px', right: '7%', transform: 'translateX(-50%)' }} color="primary" aria-label="add" size="small"
-          onClick={handleClick}
+          onClick={handleClick('top-end')}
         >
           <lord-icon
             src="https://cdn.lordicon.com/nocovwne.json"
@@ -39,19 +45,15 @@ export default function CopyRightPage() {
           </lord-icon>
         </Fab>
       </Tooltip>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile3</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+      <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Typography sx={{ p: 2 }}>All of the music list is come from official youtube and this website is only for fun. Enjoy!</Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
     </>
   );
 }
